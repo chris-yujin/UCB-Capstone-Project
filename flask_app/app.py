8eb3a11758abace914e2a6f2b5d256ed67a4758a
chris-yujin <cyheng@ucdavis.edu>
2023-10-02 12:43:49 -0700

    added aved models to front end

---
+++ flask_app/app.py
@@ -0,0 +1,148 @@
+# Import the dependencies
+import pandas as pd
+import numpy as np
+import datetime as dt
+
+from flask import Flask, redirect, url_for, render_template, jsonify
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, text
+import psycopg2
+
+# import postgres credentials
+from config import postgres_username, db_password
+
+# You can put your ML functions somewhere out here
+# import testimport as t
+import pickle_models as pkl
+
+#################################################
+# Database Setup
+#################################################
+from etl_pipeline import load 
+
+# a = t.build_blobs()
+# model = t.train_model(a)
+# a2 = t.view_clusters(model, a)
+clean_df = load.loadcleandata()
+
+#################################################
+# Read in saved models
+#################################################
+o_model = pkl.read_om_model()
+c_model = pkl.read_c_model()
+d_model = pkl.read_d_model()
+# new_input = new_input = [[4.121429,24.8,18.4, 44.0, 73.2, 33.0, 36.1, 16.9, 28.8, 35.5, 41.0]]
+
+# Create SQLAlchemy engine to connect to Postgresql Database
+
+
+engine = create_engine(f"postgresql+psycopg2://{postgres_username}:{db_password}@localhost:5432/test_db")
+
+# Convert clean_df dataframe to sql table                                   
+clean_df.to_sql('datapoints', engine, index=False, if_exists="replace")
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+# We can nest other functions within the first function to do some fancy stuff
+@app.route("/")
+def loader():
+    # Start at loading page
+    return render_template("loader.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+# @app.route("/")
+# def home():
+#     return render_template("home.html")
+@app.route("/dataload")
+def dataload():
+    print("Function Started")
+    load.timewaster()
+    print("Function Ended")
+    return redirect(url_for("home"))
+
+@app.route("/predict/<a>/<b>/<c>/<d>/<e>/<f>/<g>/<h>/<i>")
+def predict(a,b,c,d,e,f,g,h,i):
+    test = float(a)+float(b)+float(c)+float(d)+float(e)+float(f)+float(g)+float(h)+float(i)
+    return jsonify(sum=round(test,2), 
+                   obesity_prevalance=float(pkl.all_predict(o_model,c_model, d_model, a,b,c,d,e,f,g,h,i)[0]),
+                   cancer_prevalance=float(pkl.all_predict(o_model,c_model, d_model, a,b,c,d,e,f,g,h,i)[1]),
+                   diabetes_prevalance=float(pkl.all_predict(o_model,c_model, d_model, a,b,c,d,e,f,g,h,i)[2]),
+                   )
+
+# @app.route("/obesity_model")
+# def om_user_input():
+#     return jsonify(om.om_predict(o_model, new_input))
+
+@app.route("/gettable")
+def gettable():
+    with engine.connect() as conn:
+        query = text("SELECT * FROM datapoints LIMIT 10")
+        results = conn.execute(query).fetchall()
+    resultlist = []
+    for row in results:
+        resultlist.append([row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]])
+
+    return jsonify(data=resultlist)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.route("/update_data/<zipcode>")
+def update_data(zipcode):
+    #convert user input as an integer
+    zipcode = int(zipcode)
+
+    with engine.connect() as conn:
+        query = text(f'SELECT * FROM datapoints WHERE "ZIPCODE" = {zipcode}')
+        results = conn.execute(query).fetchall()
+
+    if not results:
+        # Handle the case where no data is found for the given zipcode
+        return jsonify(error="No data found for the provided zipcode")
+
+    # Process the results and extract the necessary values
+    data = results[0]
+    state = data[1]
+    income_per_individual = data[2]
+    lacking_health_insurance = data[3]
+    binge_drinking = data[4]
+    high_blood_pressure = data[5]
+    routine_check_ups = data[6]
+    currently_smoking = data[7]
+    depressed = data[8]
+    no_leisure_physical_activity =data[9]
+    less_7_hours_sleep = data[10]
+    
+
+    # Return the extracted data as JSON
+    return jsonify(
+        zipcode=zipcode,
+        state = state,
+        income_per_individual=income_per_individual,
+        lacking_health_insurance=lacking_health_insurance,
+        binge_drinking = binge_drinking,
+        high_blood_pressure = high_blood_pressure,
+        routine_check_ups = routine_check_ups,
+        currently_smoking = currently_smoking,
+        depressed = depressed,
+        no_leisure_physical_activity = no_leisure_physical_activity,
+        less_7_hours_sleep = less_7_hours_sleep
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ flask_app/static/js/logic.js
@@ -0,0 +1,111 @@
+function testFunction() {
+    console.log("Page loaded!")
+    window.location.assign("/dataload");
+}
+
+function writeTable(){
+    d3.json("/gettable").then(function(data){
+        let content = data.data;
+        let selector = d3.select("tbody");
+        content.forEach((element)=>{
+            selector.append("tr").html(
+                `
+                <td>${element[0]}</td>
+                <td>${element[1]}</td>
+                <td>${Math.round(element[2] * 100) / 100}</td>
+                <td>${Math.round(element[3] * 100) / 100}</td>
+                <td>${Math.round(element[4] * 100) / 100}</td>
+                <td>${Math.round(element[5] * 100) / 100}</td>
+                <td>${Math.round(element[6] * 100) / 100}</td>
+                <td>${Math.round(element[7] * 100) / 100}</td>
+                <td>${Math.round(element[8] * 100) / 100}</td>
+                <td>${Math.round(element[9] * 100) / 100}</td>
+                <td>${Math.round(element[10] * 100) / 100}</td>
+                `
+            )
+        })
+    })
+}
+
+d3.selectAll("input").on("change", function(event){
+    d3.select("#"+event.target.id+"l").text(event.target.name+": "+event.target.value)
+    updateValue()
+})
+
+function updateValue(){
+    a = d3.select("#customRange1").property("value")
+    b = d3.select("#customRange2").property("value")
+    c = d3.select("#customRange3").property("value")
+    d = d3.select("#customRange4").property("value")
+    e = d3.select("#customRange5").property("value")
+    f = d3.select("#customRange6").property("value")
+    g = d3.select("#customRange7").property("value")
+    h = d3.select("#customRange8").property("value")
+    i = d3.select("#customRange9").property("value")
+    d3.json("/predict/"+a+"/"+b+"/"+c+"/"+d+"/"+e+"/"+f+"/"+g+"/"+h+"/"+i).then(function(data){
+        console.log(data)
+        d3.select("#dynamic_cancer").text(data["cancer_prevalance"])
+        d3.select("#dynamic_diabetes").text(data["diabetes_prevalance"])
+        d3.select("#dynamic_obesity").text(data["obesity_prevalance"])
+        d3.select("#dynamic2").text(data["sum"])
+    })
+}
+
+// Capture user input (zipcode) and trigger the update
+d3.select("#text-input").on("change", function () {
+    const zipcode = d3.select(this).property("value");
+    
+    
+    // Send an AJAX request to the Flask route
+    d3.json(`/update_data/${zipcode}`).then(function (data) {
+        if (data.error) {
+            // Handle the case where no data is found for the zipcode
+            console.error(data.error);
+            // Display a message to the user
+            d3.select("#user-input-display").html("<strong>Zipcode Not Found</strong>");
+            // Update sliders and table with default values or clear them
+        } else {
+            
+            // Display the user input
+            d3.select("#user-input-display").text(zipcode);
+            // Update the table with the fetched data
+            d3.select("tbody").html("");
+            console.log(data);
+            
+            let selector = d3.select("tbody");
+            
+            selector.append("tr").html(
+                `
+                <td>${data["zipcode"]}</td>
+                <td>${data["state"]}</td>
+                <td>${Math.round(data["income_per_individual"] *100) / 100}</td>
+                <td>${Math.round(data["lacking_health_insurance"] * 100) / 100}</td>
+                <td>${Math.round(data["binge_drinking"] * 100) / 100}</td>
+                <td>${Math.round(data["high_blood_pressure"] * 100) / 100}</td>
+                <td>${Math.round(data["routine_check_ups"] * 100) / 100}</td>
+                <td>${Math.round(data["currently_smoking"] * 100) / 100}</td>
+                <td>${Math.round(data["depressed"] * 100) / 100}</td>
+                <td>${Math.round(data["no_leisure_physical_activity"] * 100) / 100}</td>
+                <td>${Math.round(data["less_7_hours_sleep"] * 100) / 100}</td>
+                
+                `
+            )
+            
+            // Update the sliders with the fetched values
+            d3.select("#customRange1").property("value", data["income_per_individual"]);
+            d3.select("#customRange2").property("value", data["lacking_health_insurance"]);
+            d3.select("#customRange3").property("value", data["binge_drinking"]);
+            d3.select("#customRange4").property("value", data["high_blood_pressure"]);
+            d3.select("#customRange5").property("value", data["routine_check_ups"]);
+            d3.select("#customRange6").property("value", data["currently_smoking"]);
+            d3.select("#customRange7").property("value", data["depressed"]);
+            d3.select("#customRange8").property("value", data["no_leisure_physical_activity"]);
+            d3.select("#customRange9").property("value", data["less_7_hours_sleep"]);
+            
+            updateValue()
+          
+            
+            
+        }
+    });
+});
